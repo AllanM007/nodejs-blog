16695d8c20aa05ebbc72556a9660db03e1a96da6
mzawadi <mwaranguallan345@gmail.com>
2021-11-04 15:53:04 +0300

    add Post model

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 const path = require('path');
 
-const expressEdge = require('express-edge');
 const express = require('express');
-
+const mongoose = require('mongoose');
 const app = new express();
 
+mongoose.connect('mongodb://localhost:27017/nodejs-blog', { useNewUrlParser: true })
+    .then(() => 'You are now connected to Mongo!')
+    .catch(err => console.error('Something went wrong', err))
+
 app.use(express.static('public'));
-app.use(expressEdge);
-app.set('views', __dirname + '/views');
 
 app.get('/', (req, res) => {
-    res.render('index');
+    res.sendFile(path.resolve(__dirname, 'pages/index.html'));
 });
 
 app.get('/post', (req, res) => {
